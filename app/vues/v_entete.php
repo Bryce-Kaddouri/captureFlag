8aed049488e934a650b7507010ae2e7d2b0cd5a3
Bryce-Kaddouri <bryce.kaddouri@gmail.com>
2022-11-07 11:03:40 +0400

    1st commit

---
+++ app/vues/v_entete.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="../styles/stylesIndex.css">
+    <!-- cdn sweetAlert2 -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script> -->
+    <script src="../node_modules/jquery/dist/jquery.js"></script>
+    <script src="../node_modules/sweetalert2/dist/sweetalert2.js"></script>
+    <link rel="stylesheet" href="../node_modules/sweetalert2/dist/sweetalert2.css">
+
+    <meta name="theme-color" media="(prefers-color-scheme: light)" content="white">
+    <!-- <meta name="theme-color" content="#29434d" /> -->
+    <meta name="msapplication-TileColor" content="#130858" />
+    <meta name="msapplication-TileImage" content="./images/icons/mstile-150x150.png" />
+    <meta name="mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="apple-mobile-web-app-title" content="La Marjoline" />
+    <meta name="application-name" content="La Marjoline" />
+    <link rel="apple-touch-icon" href="./images/icons/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" href="./images/icons/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="./images/icons/favicon-16x16.png" sizes="16x16" />
+    <link rel="manifest" href="./manifest.json" />
+    <style>
+        @media screen and (display-mode: standalone) {
+            header {
+                height: 0 !important;
+            }
+        }
+    </style>
+
+
+    <!-- <link rel="stylesheet" href="styles/boutton.scss"> -->
+    <!-- cdn jquery-->
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
+
+</head>
+
+<body>
